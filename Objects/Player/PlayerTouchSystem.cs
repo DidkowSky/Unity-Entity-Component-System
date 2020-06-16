1b7a5ff7ac10cc8adf54cdcc384a5e6c0ea8a603
DidkowSky <dabrowskidawid01@gmail.com>
2020-06-16 22:31:43 +0200

    Added UNITY_EDITOR directive block for debugging purposes in editor play mode

--- Objects/Player/PlayerTouchSystem.cs
+++ Objects/Player/PlayerTouchSystem.cs
@@ -21,15 +21,39 @@ namespace Assets.Scripts.DOTS
 
         protected override void OnUpdate()
         {
+#if UNITY_EDITOR
+            if (Input.GetMouseButton(0))
+            {
+                if (cameraComponent != null)
+                {
+                    Vector3 worldTouchPosition = cameraComponent.ScreenToWorldPoint(Input.mousePosition);
+                    worldTouchPosition.z = 0;
+                    SetPlayerPosition(worldTouchPosition);
+                }
+                else
+                {
+                    Debug.LogWarning("CameraComponent is null!");
+                }
+
+                if (Input.GetMouseButtonDown(1))
+                {
+                    StartShooting();
+                }
+                if (Input.GetMouseButtonUp(1))
+                {
+                    StopShooting();
+                }
+            }
+#endif
             if (Input.touches.Length > 0)
             {
                 if (Input.touches[0].phase == TouchPhase.Began)
                 {
-                    SetPlayersPosition(Input.touches[0]);
+                    SetPlayerPosition(Input.touches[0]);
                 }
                 else if (Input.touches[0].phase == TouchPhase.Moved)
                 {
-                    SetPlayersPosition(Input.touches[0]);
+                    SetPlayerPosition(Input.touches[0]);
                 }
                 else if (Input.touches[0].phase == TouchPhase.Ended)
                 {
@@ -50,28 +74,44 @@ namespace Assets.Scripts.DOTS
             }
         }
 
-        private void SetPlayersPosition(Touch touch)
+        private void SetPlayerPosition(Vector3 position)
         {
-            Entities.WithAll<PlayerComponent>().ForEach((ref PlayerComponent playerComponent) =>
-            {
-                playerComponent.DestinationPoint = GetTouchPosition(touch);
-            });
+            Entities
+                .WithAll<PlayerComponent>()
+                .ForEach((ref PlayerComponent playerComponent) =>
+                {
+                    playerComponent.DestinationPoint = position;
+                });
+        }
+
+        private void SetPlayerPosition(Touch touch)
+        {
+            Entities
+                .WithAll<PlayerComponent>()
+                .ForEach((ref PlayerComponent playerComponent) =>
+                {
+                    playerComponent.DestinationPoint = GetTouchPosition(touch);
+                });
         }
 
         private void StartShooting()
         {
-            Entities.WithAll<PlayerWeaponComponent>().ForEach((ref PlayerWeaponComponent weaponComponent) =>
-            {
-                weaponComponent.isShooting = true;
-            });
+            Entities
+                .WithAll<PlayerWeaponComponent>()
+                .ForEach((ref PlayerWeaponComponent weaponComponent) =>
+                {
+                    weaponComponent.isShooting = true;
+                });
         }
 
         private void StopShooting()
         {
-            Entities.WithAll<PlayerWeaponComponent>().ForEach((ref PlayerWeaponComponent weaponComponent) =>
-            {
-                weaponComponent.isShooting = false;
-            });
+            Entities
+                .WithAll<PlayerWeaponComponent>()
+                .ForEach((ref PlayerWeaponComponent weaponComponent) =>
+                {
+                    weaponComponent.isShooting = false;
+                });
         }
 
         private float3 GetTouchPosition(Touch touch)
